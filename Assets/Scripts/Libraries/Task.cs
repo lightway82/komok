25301c25109a87b72294c4df2ad02dc48044be15
lightway82 <lightway82@gmail.com>
2019-03-05 13:13:53 +0300

    TaskManager для выполнения заданий асинхронно в очереди с приоритетом

---
+++ Assets/Scripts/Libraries/Task.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public enum TaskPriorityEnum
+{
+    Default,
+    High,
+    Interrupt
+}
+public interface ITask
+{
+    TaskPriorityEnum Priority { get; }
+
+    void Start();
+    ITask Subscribe(Action feedback); 
+    void Stop();
+}
+
+
+public class Task : ITask
+{
+    public TaskPriorityEnum Priority => _taskPriority;
+
+    private TaskPriorityEnum _taskPriority = TaskPriorityEnum.Default;
+
+    private Action _feedback;
+    private MonoBehaviour _coroutineHost;
+    private Coroutine _coroutine;
+    private IEnumerator _taskAction;
+
+    public static Task Create(IEnumerator taskAction, MonoBehaviour coroutineHost,  TaskPriorityEnum priority = TaskPriorityEnum.Default)
+    {
+        return new Task(taskAction,coroutineHost,  priority);
+    }
+
+    public Task(IEnumerator taskAction,  MonoBehaviour coroutineHost, TaskPriorityEnum priority = TaskPriorityEnum.Default)
+    {
+        _coroutineHost = coroutineHost;
+        _taskPriority = priority;
+        _taskAction = taskAction;
+    }
+
+    public void Start()
+    {
+        if (_coroutine == null)
+        {
+            _coroutine = _coroutineHost.StartCoroutine(RunTask());
+        }
+    }
+
+    public void Stop()
+    {
+        if (_coroutine == null) return;
+        _coroutineHost.StopCoroutine(_coroutine);
+        _coroutine = null;
+    }
+
+    public ITask Subscribe(Action feedback)
+    {
+        _feedback += feedback;
+
+        return this;
+    }
+
+
+    private IEnumerator RunTask()
+    {
+        yield return _taskAction;
+
+        CallSubscribe();
+    }
+
+    private void CallSubscribe()
+    {
+        _feedback?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Libraries/TaskManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO возврат ошибок, возвращаемые значения, передаваемые значение в таску.
+/// <summary>
+/// Позволяет выполнять таски в очереди по приоритетам.
+/// Довольно простом, можно совершенствовать.
+/// Требует экземпляр MonoBehaviour для запуска
+/// </summary>
+public class TaskManager
+{
+    public ITask CurrentTask => _currentTask;
+
+    private ITask _currentTask;
+    private List<ITask> _tasks = new List<ITask>();
+
+
+    private MonoBehaviour coroutineHost;
+
+    public TaskManager(MonoBehaviour coroutineHost)
+    {
+        this.coroutineHost = coroutineHost;
+    }
+
+    public void AddTask(IEnumerator taskAction, Action callback, 
+        TaskPriorityEnum taskPriority = TaskPriorityEnum.Default)
+    {
+        var task = Task.Create(taskAction, coroutineHost, taskPriority).Subscribe(callback);
+
+        ProcessingAddedTask(task, taskPriority);
+    }
+
+    public void Break()
+    {
+        _currentTask?.Stop();
+    }
+
+    public void Restore()
+    {
+        TaskQueueProcessing();
+    }
+
+    public void Clear()
+    {
+        Break();
+
+        _tasks.Clear();
+    }
+
+    private void ProcessingAddedTask(ITask task, TaskPriorityEnum taskPriority)
+    {
+        switch (taskPriority)
+        {
+            case TaskPriorityEnum.Default:
+                _tasks.Add(task);
+            break;
+            case TaskPriorityEnum.High:
+                _tasks.Insert(0, task);
+            break;
+            case TaskPriorityEnum.Interrupt:
+                if (_currentTask != null && _currentTask.Priority != TaskPriorityEnum.Interrupt)
+                {
+                    _currentTask.Stop();
+                }
+                _currentTask = task;
+                task.Subscribe(TaskQueueProcessing).Start();
+            break;
+        }
+
+        if (_currentTask != null) return;
+        _currentTask = GetNextTask();
+        _currentTask?.Subscribe(TaskQueueProcessing).Start();
+    }
+
+    private void TaskQueueProcessing()
+    {
+        _currentTask = GetNextTask();
+        _currentTask?.Subscribe(TaskQueueProcessing).Start();
+    }
+
+    private ITask GetNextTask()
+    {
+        if (_tasks.Count <= 0) return null;
+        var returnValue = _tasks[0];
+        _tasks.RemoveAt(0);
+
+        return returnValue;
+    }
+}
